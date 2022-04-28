bee31abbe89ce2fbada2213907aa0b2c87b8c034
asfand687 <asfandyar687@gmail.com>
2022-04-28 20:42:56 +0500

    made changes to the slider

--- src/components/Slick.jsx
+++ src/components/Slick.jsx
@@ -11,6 +11,7 @@ import Shadow from '../assets/shadow.png'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Marker from './Marker'
+import { motion } from 'framer-motion'
 
 
 
@@ -49,7 +50,7 @@ const Slick = () => {
 
   const NextArrow = ({ onClick }) => {
     return (
-      <div className="arrow next" onClick={onClick}>
+      <div className="absolute top-[50%] z-10 right-0 bg-white rounded-full transition-opacity ease-in p-2 hover:opacity-90 cursor-pointer" onClick={onClick}>
         <img src={ArrowRight} alt="next" />
       </div>
     );
@@ -57,8 +58,8 @@ const Slick = () => {
 
   const PrevArrow = ({ onClick }) => {
     return (
-      <div className="arrow prev" onClick={onClick}>
-        <img src={ArrowLeft} alt="prev" />
+      <div className="absolute top-[50%] z-10 left-0 bg-white rounded-full transition-opacity ease-in p-2 hover:opacity-90 cursor-pointer" onClick={onClick}>
+        <img src={ArrowLeft} alt="next" />
       </div>
     );
   };
@@ -98,20 +99,24 @@ const Slick = () => {
   };
 
   return (
-    <div className="bg-gray-600">
+    <div className="bg-black">
       <div className="px-6 lg:block">
         <Slider {...settings}>
           {sliderData.map((slider, idx) => (
-            <div key={slider.id} className="px-4">
+            <div key={slider.id} className={`px-4 ${idx === imageIndex ? "" : "opacity-30"}`} >
               <div className="flex flex-col items-center lg:flex-row space-x-6 py-14">
                 <article className={` relative`}>
-                  <img className={`absolute z-50  left-1/2 transform -translate-x-1/2 ${idx === imageIndex ? "animate-pulse -top-14 scale-125" : ""}`} src={Shadow} alt="shadow" />
-                  <img className="w-[250px] h-[330px] relative z-10" src={slider.img} alt="nft" />
+                  {idx === imageIndex && (
+                    <img className={`absolute z-50  left-1/2 transform -translate-x-1/2 hidden ${idx === imageIndex ? "animate-pulse -top-14 scale-125 block" : ""}`} src={Shadow} alt="shadow" />
+                  )}
+                  <motion.img animate={{ scale: idx === imageIndex ? 1 : 0.9 }} transition={{ delay: 0.2, ease: 'easeInOut' }} className="w-[250px] h-[330px] relative z-10" src={slider.img} alt="nft" />
                 </article>
                 <article className="flex-1">
                   <Marker classes="mt-4" number={slider.id} small />
-                  <h2 className="text-white pt-2">{slider.title}</h2>
-                  <p className="text-white text-justify text-[12px] max-w-[300px]">{slider.description}</p>
+                  <motion.div initial={{ opacity: 0.4 }} animate={{ opacity: idx === imageIndex ? 1 : 0.4 }} transition={{ delay: 0.5, ease: 'circIn' }}>
+                    <h2 className="text-white pt-2">{slider.title}</h2>
+                    <p className="text-white text-justify text-[12px] max-w-[300px]">{slider.description}</p>
+                  </motion.div>
                 </article>
               </div>
             </div>
