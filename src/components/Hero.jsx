036baf714f6d20bb3e4ed5df52a285118e417943
asfand687 <asfandyar687@gmail.com>
2022-05-02 00:58:22 +0500

    added hero video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import ReactPlayer from 'react-player'
+import Video from '../assets/hero.mp4'
 
 const Hero = () => {
   return (
     <section className="w-full max-w-[1300px] mx-auto px-4 h-[50vh] md:h-[80vh] ">
-      <div className="bg-black w-full h-full flex justify-center items-center">
-        <h2 className="text-white">Video</h2>
-      </div>
+      <ReactPlayer
+        width="100%"
+        height="100%"
+        url={Video}
+        muted
+        playing={true}
+        controls={true}
+        loop={true}
+      />
     </section>
   )
 }
