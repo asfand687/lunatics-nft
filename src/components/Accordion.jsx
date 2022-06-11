b9461662774c81bf142f0df8b497c3e925e70161
asfand687 <asfandyar687@gmail.com>
2022-06-12 01:46:12 +0500

    added the markers to the faq component

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import Marker from './Marker'
 import Chevron from '../assets/chevron-down.svg'
 
 const Accordion = ({ faq }) => {
@@ -10,7 +9,7 @@ const Accordion = ({ faq }) => {
       {
         show && (
           <article className="flex space-x-6 py-8">
-            <Marker number={faq.id} />
+            <img className="w-20" src={faq.marker} alt={`marker for ${faq.id}`} />
             <div className="flex-1">
               <h2 className="text-3xl font-medium pb-4">
                 {faq.title}
--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -1,51 +1,69 @@
 import Accordion from '../components/Accordion'
+import Marker1 from '../assets/marker-001.svg'
+import Marker2 from '../assets/marker-002.svg'
+import Marker3 from '../assets/marker-003.svg'
+import Marker4 from '../assets/marker-004.svg'
+import Marker5 from '../assets/marker-005.svg'
+import Marker6 from '../assets/marker-006.svg'
+import Marker7 from '../assets/marker-007.svg'
+import Marker8 from '../assets/marker-008.svg'
+import Marker9 from '../assets/marker-009.svg'
 
 const faqData = [
   {
     id: "01",
     title: "Question 1",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit eget elementum arcu, purus in at. Tellus sed feugiat enim, eget porta est dui. Vel in sit arcu nec quis eu. Amet lacus, ornare massa et orci. Morbi blandit diam nisl tempor rhoncus elit malesuada.",
-    open: true
+    open: true,
+    marker: Marker1
   },
   {
     id: "02",
     title: "Question 2",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit eget elementum arcu, purus in at. Tellus sed feugiat enim, eget porta est dui. Vel in sit arcu nec quis eu. Amet lacus, ornare massa et orci. Morbi blandit diam nisl tempor rhoncus elit malesuada."
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit eget elementum arcu, purus in at. Tellus sed feugiat enim, eget porta est dui. Vel in sit arcu nec quis eu. Amet lacus, ornare massa et orci. Morbi blandit diam nisl tempor rhoncus elit malesuada.",
+    marker: Marker2
   },
   {
     id: "03",
     title: "Question 3",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit eget elementum arcu, purus in at. Tellus sed feugiat enim, eget porta est dui. Vel in sit arcu nec quis eu. Amet lacus, ornare massa et orci. Morbi blandit diam nisl tempor rhoncus elit malesuada."
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit eget elementum arcu, purus in at. Tellus sed feugiat enim, eget porta est dui. Vel in sit arcu nec quis eu. Amet lacus, ornare massa et orci. Morbi blandit diam nisl tempor rhoncus elit malesuada.",
+    marker: Marker3
   },
   {
     id: "04",
     title: "Question 4",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit eget elementum arcu, purus in at. Tellus sed feugiat enim, eget porta est dui. Vel in sit arcu nec quis eu. Amet lacus, ornare massa et orci. Morbi blandit diam nisl tempor rhoncus elit malesuada."
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit eget elementum arcu, purus in at. Tellus sed feugiat enim, eget porta est dui. Vel in sit arcu nec quis eu. Amet lacus, ornare massa et orci. Morbi blandit diam nisl tempor rhoncus elit malesuada.",
+    marker: Marker4
   },
   {
     id: "05",
     title: "Question 5",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit eget elementum arcu, purus in at. Tellus sed feugiat enim, eget porta est dui. Vel in sit arcu nec quis eu. Amet lacus, ornare massa et orci. Morbi blandit diam nisl tempor rhoncus elit malesuada."
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit eget elementum arcu, purus in at. Tellus sed feugiat enim, eget porta est dui. Vel in sit arcu nec quis eu. Amet lacus, ornare massa et orci. Morbi blandit diam nisl tempor rhoncus elit malesuada.",
+    marker: Marker5
   },
   {
     id: "06",
     title: "Question 6",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit eget elementum arcu, purus in at. Tellus sed feugiat enim, eget porta est dui. Vel in sit arcu nec quis eu. Amet lacus, ornare massa et orci. Morbi blandit diam nisl tempor rhoncus elit malesuada."
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit eget elementum arcu, purus in at. Tellus sed feugiat enim, eget porta est dui. Vel in sit arcu nec quis eu. Amet lacus, ornare massa et orci. Morbi blandit diam nisl tempor rhoncus elit malesuada.",
+    marker: Marker6
   },
   {
     id: "07",
     title: "Question 7",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit eget elementum arcu, purus in at. Tellus sed feugiat enim, eget porta est dui. Vel in sit arcu nec quis eu. Amet lacus, ornare massa et orci. Morbi blandit diam nisl tempor rhoncus elit malesuada."
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit eget elementum arcu, purus in at. Tellus sed feugiat enim, eget porta est dui. Vel in sit arcu nec quis eu. Amet lacus, ornare massa et orci. Morbi blandit diam nisl tempor rhoncus elit malesuada.",
+    marker: Marker7
   },
   {
     id: "08",
     title: "Question 8",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit eget elementum arcu, purus in at. Tellus sed feugiat enim, eget porta est dui. Vel in sit arcu nec quis eu. Amet lacus, ornare massa et orci. Morbi blandit diam nisl tempor rhoncus elit malesuada."
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit eget elementum arcu, purus in at. Tellus sed feugiat enim, eget porta est dui. Vel in sit arcu nec quis eu. Amet lacus, ornare massa et orci. Morbi blandit diam nisl tempor rhoncus elit malesuada.",
+    marker: Marker8
   },
   {
     id: "09",
     title: "Question 9",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit eget elementum arcu, purus in at. Tellus sed feugiat enim, eget porta est dui. Vel in sit arcu nec quis eu. Amet lacus, ornare massa et orci. Morbi blandit diam nisl tempor rhoncus elit malesuada."
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit eget elementum arcu, purus in at. Tellus sed feugiat enim, eget porta est dui. Vel in sit arcu nec quis eu. Amet lacus, ornare massa et orci. Morbi blandit diam nisl tempor rhoncus elit malesuada.",
+    marker: Marker9
   },
 ]
 
