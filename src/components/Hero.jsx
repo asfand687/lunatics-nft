c9afe9e2847716493ffe14d8097222c61b650e96
asfand687 <asfandyar687@gmail.com>
2022-08-03 20:29:31 +0500

    changed the height of clouds

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,17 +11,17 @@ const Hero = () => {
     <>
       <section
         ref={ref}
-        className="h-[70vh] md:h-screen text-white hero-bg flex items-end relative w-full">
-        <img className={`hidden md:block relative -bottom-56 h-[350px] w-full z-10 transition-opacity ease-in-out duration-300 ${inView ? 'opacity-100' : 'opacity-0'}`} src={Clouds} alt="clouds" />
+        className=" h-[70vh] md:h-screen text-white hero-bg flex items-end relative w-full">
+        <img className={`hidden md:block relative -bottom-56  w-full z-10 transition-opacity ease-in-out duration-300 ${inView ? 'opacity-100' : 'opacity-0'}`} src={Clouds} alt="clouds" />
       </section>
 
       <section
         ref={ref2}
-        className="h-[70vh] md:h-screen text-white flex items-end  hero-bg second relative w-full">
-        <img className={`hidden md:block relative -bottom-56 h-[350px] w-full z-10 transition-opacity ease-in-out duration-300 ${inView2 ? 'opacity-100' : 'opacity-0'}`} src={Clouds} alt="clouds" />
+        className=" h-[70vh] md:h-screen text-white flex items-end  hero-bg second relative w-full">
+        <img className={`hidden md:block relative -bottom-56  w-full z-10 transition-opacity ease-in-out duration-300 ${inView2 ? 'opacity-100' : 'opacity-0'}`} src={Clouds} alt="clouds" />
       </section>
 
-      <section className="h-[70vh] md:h-screen text-white hero-bg third relative w-full">
+      <section className=" h-[70vh] md:h-screen text-white hero-bg third relative w-full">
       </section>
     </>
   )
