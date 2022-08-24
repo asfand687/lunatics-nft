6e5276f937098ed9cfbcde8741317c37066a6d9a
asfand687 <asfandyar687@gmail.com>
2022-08-24 23:32:02 +0500

    added clouds and fade in hero section of homepage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
-import React,{useEffect, useRef, useState} from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import Clouds from '../assets/clouds.png'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -10,18 +11,18 @@ const Hero = () => {
   const scroll = (e) => {
 
     if (slider === null)
-        return 0;
+      return 0;
 
-    if(e.wheelDelta > 0) {
+    if (e.wheelDelta > 0) {
       slider.current.slickNext()
     } else {
-      window.scroll(0,0)
+      window.scroll(0, 0)
       slider.current.slickPrev()
     }
   }
 
-
   const settings = {
+    fade: true,
     lazyLoad: true,
     speed: 300,
     slidesToShow: 1,
@@ -33,31 +34,31 @@ const Hero = () => {
   }
 
   console.log(currentSlide)
- 
+
   useEffect(() => {
-    window.addEventListener("wheel", scroll,true);
+    window.addEventListener("wheel", scroll, true);
 
     return () => {
-        window.removeEventListener("wheel", scroll, true);
+      window.removeEventListener("wheel", scroll, true);
     };
   }, [])
 
   return (
     <div>
       <Slider {...settings} ref={slider}>
-      <section
-        className="snap-center h-screen text-white hero-bg flex items-end relative w-full">
-        
-      </section>
+        <section
+          className="snap-center h-screen text-white hero-bg flex items-end relative w-full">
+          <img className={`hidden md:block relative top-96  w-full z-10 transition-opacity ease-in-out duration-300 ${currentSlide === 0 ? "opacity-80" : 'opacity-0'}`} src={Clouds} alt="clouds" />
+        </section>
 
-      <section
-        className="snap-start scrollPad md:h-screen text-white flex items-end  hero-bg second relative w-full">
-        
-      </section>
+        <section
+          className="snap-start scrollPad md:h-screen text-white flex items-end  hero-bg second relative w-full">
+          <img className={`hidden md:block relative top-96  w-full z-10 transition-opacity ease-in-out duration-300 ${currentSlide === 1 ? "opacity-80" : 'opacity-0'}`} src={Clouds} alt="clouds" />
+        </section>
 
-      <section className="snap-center h-screen md:h-screen text-white hero-bg third relative w-full">
-      </section>
-    </Slider>
+        <section className="snap-center h-screen md:h-screen text-white hero-bg third relative w-full">
+        </section>
+      </Slider>
     </div>
   )
 }
