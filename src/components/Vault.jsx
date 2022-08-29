fb2d96a913ad70c52b14d3bf31af51a9826f5960
asfand687 <asfandyar687@gmail.com>
2022-08-30 02:13:47 +0500

    reduced the input size for mobile screen homepage

--- src/components/Vault.jsx
+++ src/components/Vault.jsx
@@ -27,8 +27,8 @@ const Vault = () => {
                     <h2 className="text-4xl font-medium tracking-[-0.1em]  pb-8">Enter Your Email To <br /> Gain Access To The Vault</h2>
                     <h3 className="text-2xl md:text-3xl tracking-[-0.1em]  pb-4 ">Get Added To Our Email List</h3>
                     <div className="relative">
-                      <input className="w-[30rem] text-lg bg-gray-200 rounded-3xl px-4 py-2 outline-none border border-transparent focus:border-gray-400" placeholder='example@gmail.com' />
-                      <button onClick={handleSubmit} className="bg-black font-sans h-[47px] font-semibold tracking-wide uppercase w-[120px] rounded-3xl text-white absolute top-0 right-0 hover:opacity-80">Submit</button>
+                      <input className="lg:w-[30rem] text-lg bg-gray-200 lg:rounded-3xl px-4 py-2 outline-none border border-transparent focus:border-gray-400" placeholder='example@gmail.com' />
+                      <button onClick={handleSubmit} className="bg-black font-sans h-[47px] font-semibold tracking-wide w-[100px] uppercase lg:w-[120px] lg:rounded-3xl text-white absolute top-0 right-8 lg:right-0 hover:opacity-80">Submit</button>
                     </div>
                   </div>
                 </article>
